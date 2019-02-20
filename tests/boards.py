c873f1e2869a27dff846519cfb88dfef80f2962c
David Roundy <roundyd@physics.oregonstate.edu>
2019-02-20 09:28:04 -0800

    enable color cycling and some basic joinery

--- tests/boards.py
+++ tests/boards.py
@@ -1,8 +1,20 @@
 import woodplotlib as wood
 
-a = wood.Board(dim = '1x2', feet = 4)
+a = wood.Board(dim = '1x2', feet = 2)
+#a.color = 'r'
 
-b = wood.Board(dim = '1x2', feet = 4)
-b.origin[0] = 4
+c = wood.Board(dim = '1x4', feet = 2)
+#c.color = 'tab:orange'
+c.join(a, 'end-to-far-left-edge')
+
+b = wood.Board(dim = '1x3', feet = 2)
+b.length -= c.width
+b.join(a, 'edge-to-left-edge')
+
+d = wood.Board(dim = '1x4', feet = 1)
+d.join(c, 'end-to-far-back')
+
+e = wood.Board(dim = '1x4', feet = 1)
+e.join(c, 'end-to-near-back')
 
 wood.show3d()
--- woodplotlib/__init__.py
+++ woodplotlib/__init__.py
@@ -3,10 +3,7 @@ import numpy as _np
 name = "woodplotlib"
 version = '0.0'
 
-class Assembly(object):
-    pass
-
-class Board(Assembly):
+class Board(object):
     """A board that may also have a location."""
     _all_boards = []
 
@@ -15,7 +12,10 @@ class Board(Assembly):
         self.width = 1.75
         self.length = 12
         self.origin = _np.array([0, 0, 0.])
-        self.color = 'k'
+        self._far = _np.array([0, 0, 1.])
+        self._left = _np.array([0, 1., 0])
+        self._front = _np.array([1., 0, 0])
+        self.color = 'C' + str(len(Board._all_boards) % 10)
         if 'dim' in kwargs:
             if kwargs['dim'] == '1x2':
                 self.width = 1.75
@@ -34,12 +34,50 @@ class Board(Assembly):
         import numpy as np
         points = np.zeros((3,8))
         points[:,0] = self.origin
-        points[:,1] = self.origin + np.array([self.width,0,0])
-        points[:,2] = self.origin + np.array([self.width,self.thickness,0])
-        points[:,3] = self.origin + np.array([0,self.thickness,0])
+        points[:,1] = self.origin + self.width*self._left
+        points[:,2] = points[:,1] + self.length*self._far
+        points[:,3] = points[:,2] - self.width*self._left
         for i in range(4):
-            points[:,4+i] = points[:,i] + np.array([0,0,self.length])
+            points[:,4+i] = points[:,i] + self.thickness*self._front
         return points
+    def join(self, board, relationship='far-left-edge-to-far-left-edge'):
+        if relationship == 'end-to-far-left-edge':
+            self.origin = board.origin + board._left*board.width + board._far*board.length
+            self._far = 1.0*board._left
+            self._left = -1.0*board._far
+            self._front = 1.0*board._front
+        elif relationship == 'left-edge-to-far-end':
+            self.origin = board.origin + board._left*board.width
+            self._far = 1.0*board._left
+            self._left = -1.0*board._far
+            self._front = 1.0*board._front
+        elif relationship == 'end-to-far-front':
+            self.origin = board.origin + board._front*board.thickness + board._far*board.length
+            self._far = 1.0*board._front
+            self._front = -1.0*board._far
+            self._left = 1.0*board._left
+        elif relationship == 'end-to-near-front':
+            self.origin = board.origin + board._front*board.thickness
+            self._far = 1.0*board._front
+            self._front = 1.0*board._far
+            self._left = 1.0*board._left
+        elif relationship == 'end-to-far-back':
+            self.origin = board.origin + board._far*board.length - board._far*self.thickness
+            self._far = -board._front
+            self._front = board._far
+            self._left = 1.0*board._left
+        elif relationship == 'end-to-near-back':
+            self.origin = board.origin + board._far*self.thickness
+            self._far = -board._front
+            self._front = -board._far
+            self._left = 1.0*board._left
+        elif relationship == 'edge-to-left-edge':
+            self.origin = board.origin + board._left*board.width
+            self._far = 1.0*board._far
+            self._left = 1.0*board._left
+            self._front = 1.0*board._front
+        else:
+            raise ValueError('Unrecognized relationship: '+ relationship)
 
 def show3d():
     import matplotlib.pyplot as plt
@@ -60,6 +98,8 @@ def show3d():
         ax.plot(corners[0,top], corners[1,top], corners[2,top], color=b.color)
         for i in range(4):
             ax.plot(corners[0,i::4], corners[1,i::4], corners[2,i::4], color=b.color)
+        plt.plot(corners[0,top[::2]], corners[1,top[::2]], corners[2,top[::2]], color=b.color)
+        plt.plot(corners[0,top[1::2]], corners[1,top[1::2]],  corners[2,top[1::2]], color=b.color)
         if corners.max() > maxdist:
             maxdist = corners.max()
         if corners.min() < mindist:
